2f10ae05a7c2d49179bd64fbcbd7475183f12762
Timofei Syromiatnikov <warfacetopic@mail.ru>
2024-02-25 03:51:51 +0400

    Updates

--- server/chunks/app/assets/ThreeDaysForecast-styles.CcYIQ1co.mjs
+++
@@ -1,6 +0,0 @@
-const ThreeDaysForecast_vue_vue_type_style_index_0_scoped_a6b56447_lang = ".section[data-v-a6b56447]{flex-direction:column}.forecast[data-v-a6b56447],.section[data-v-a6b56447]{align-items:center;display:flex;gap:20px}.forecast[data-v-a6b56447]{justify-content:center;list-style:none}.forecast li[data-v-a6b56447]{align-items:center;border-radius:12px;cursor:pointer;display:flex;flex-direction:column;padding:15px}.forecast li.active[data-v-a6b56447],li[data-v-a6b56447]:hover{box-shadow:0 3px 10px rgba(0,0,0,.2)}";
-
-const ThreeDaysForecastStyles_CcYIQ1co = [ThreeDaysForecast_vue_vue_type_style_index_0_scoped_a6b56447_lang];
-
-export { ThreeDaysForecastStyles_CcYIQ1co as default };
-//# sourceMappingURL=ThreeDaysForecast-styles.CcYIQ1co.mjs.map
